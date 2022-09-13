fa581336b736c49fa1a37b0fb68796a7268a2def
Alexander-RMIT <82481042+Alexander-RMIT@users.noreply.github.com>
2022-09-13 21:59:36 +1000

    Create createUser.dart

---
+++ FrontEnd/neighborhood_doctor/lib/pages/admin/createUser.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class CreateUser extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return CreateUserState();
+  }
+}
+
+
+class CreateUserState extends State<CreateUser> {\
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return
+  
+  
+  // }
+}
