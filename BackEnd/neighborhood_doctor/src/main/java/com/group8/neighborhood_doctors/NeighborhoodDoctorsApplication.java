2a6d9e4342b98e5f011e5f3ecdddf5c277803b04
Alexander-RMIT <82481042+Alexander-RMIT@users.noreply.github.com>
2022-09-14 22:50:40 +1000

    Update NeighborhoodDoctorsApplication.java

--- BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/NeighborhoodDoctorsApplication.java
+++ BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/NeighborhoodDoctorsApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.jdbc.core.JdbcTemplate;
 
+// Added to deal with CORS error on desktop
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 // implements CommandLineRunner
 @SpringBootApplication
@@ -13,5 +19,16 @@ public class NeighborhoodDoctorsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NeighborhoodDoctorsApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+		    @Override
+		    public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**").allowedOrigins("*");
+		    }
+		};
 	}
 }
