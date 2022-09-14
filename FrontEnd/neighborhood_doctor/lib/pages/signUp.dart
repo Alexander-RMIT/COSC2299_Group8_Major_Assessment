557c1beb99e3e20cbe250f0f6b0ba25efdabb7c2
Alexander-RMIT <82481042+Alexander-RMIT@users.noreply.github.com>
2022-09-14 17:02:40 +1000

    Update signUp.dart

--- FrontEnd/neighborhood_doctor/lib/pages/signUp.dart
+++ FrontEnd/neighborhood_doctor/lib/pages/signUp.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:email_validator/email_validator.dart';
+//import 'package:neighborhood_doctors/Model/AdminModel.dart';
+import 'package:neighborhood_doctors/Model/PatientModel.dart';
 import 'package:neighborhood_doctors/pages/login.dart';
+import 'package:http/http.dart' as http;
 import './login.dart';
+import 'dart:convert';
 
 // https://flutterawesome.com/login-ui-made-with-flutter/
 // https://github.com/hawier-dev/flutter-login-ui/blob/main/lib/main.dart
@@ -17,9 +21,53 @@ class SignUp extends StatefulWidget{
 }
 
 
+Future<PatientModel> userSignUp(String firstname, String lastname, String nameother, int age,
+  String gender,
+  String address,
+  String phonenumber,
+  String email,
+  String password,
+  BuildContext context) async {
+  Uri Url = "http://localhost:8080/patient/createPatient" as Uri;
+  var response = await http.post(Url,
+    headers: <String, String>{"Content-Type": "application/json"},
+    body: jsonEncode(<String, dynamic>{
+      "firstname": firstname,
+      "lastname": lastname,
+      "nameother": nameother,
+      "age": age,
+      "gender": gender,
+      "address": address,
+      "phoneNumber": phonenumber,
+      "email": email,
+      "password": password,
+    }));
+
+  String strResponse = response.body;
+  if (response.statusCode == 200) {
+    showDialog(
+      context: context, 
+      barrierDismissible: true,
+      builder: (BuildContext dialogContext) {
+        return ResponseAlertDialog(title: 'Backend response', content: response.body);
+      },
+    );
+  }
+
+  throw NullThrownError();
+}
+
 class SignUpState extends State<SignUp> {
   final _formKey = GlobalKey<FormState>();
   var rememberValue = false;
+
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  //TextEditingController nameOtherController = TextEditingController();
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -49,6 +97,7 @@ class SignUpState extends State<SignUp> {
                     children: [
                       Expanded(
                         child: TextFormField(
+                          controller: firstNameController,
                           // validator: (value) => EmailValidator.validate(value!)
                           //     ? null
                           //     : "Please enter a valid email",
@@ -67,6 +116,7 @@ class SignUpState extends State<SignUp> {
                       ),
                       Expanded(
                         child: TextFormField(
+                          controller: lastNameController,
                           // validator: (value) => EmailValidator.validate(value!)
                           //     ? null
                           //     : "Please enter a valid email",
@@ -86,6 +136,7 @@ class SignUpState extends State<SignUp> {
                     height: 20,
                   ),
                   TextFormField(
+                    controller: emailController,
                     validator: (value) => EmailValidator.validate(value!)
                         ? null
                         : "Please enter a valid email",
@@ -102,6 +153,7 @@ class SignUpState extends State<SignUp> {
                     height: 20,
                   ),
                   TextFormField(
+                    controller: passwordController,
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return 'Please enter your password';
@@ -122,8 +174,34 @@ class SignUpState extends State<SignUp> {
                     height: 20,
                   ),
                   ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {}
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        String email = emailController.text;
+                        String password = passwordController.text;
+                        String firstname = firstNameController.text;
+                        String lastname = lastNameController.text;
+
+                        String nameother = "testnameother";
+                        int age = 999;
+                        String gender = "male";
+                        String address = "99 testing street";
+                        String phonenumber = "1234567890";
+
+
+
+                        PatientModel newPatient = 
+                          await userSignUp(firstname, lastname, nameother, age, gender, address, phonenumber, email, password, context);
+                        
+                        emailController.text = '';
+                        passwordController.text = '';
+                        firstNameController.text = '';
+                        lastNameController.text = '';
+
+                        setState(() {
+                          PatientModel patientModel = newPatient;
+                        });
+                      }
+                    
                     },
                     style: ElevatedButton.styleFrom(
                       padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
@@ -161,6 +239,31 @@ class SignUpState extends State<SignUp> {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ResponseAlertDialog extends StatelessWidget {
+  final String title;
+  final String content;
+  final List<Widget> actions;
+
+  ResponseAlertDialog({
+    required this.title,
+    required this.content,
+    this.actions = const [],
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+        this.title,
+      ),
+      actions: this.actions,
+      content: Text(
+        this.content,
       ),
     );
   }
