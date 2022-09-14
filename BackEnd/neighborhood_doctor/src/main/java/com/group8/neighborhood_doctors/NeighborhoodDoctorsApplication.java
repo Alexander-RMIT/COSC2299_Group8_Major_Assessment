f59e15f89a1f4374de9af00882117c26bbdce742
Chrisio_Gwaan <chris322322@gmail.com>
2022-09-14 23:28:48 +1000

    Replace deprecated function for backend config

--- BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/NeighborhoodDoctorsApplication.java
+++ BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/NeighborhoodDoctorsApplication.java
@@ -1,34 +1,12 @@
 package com.group8.neighborhood_doctors;
 
-// import ch.qos.logback.core.net.SyslogOutputStream;
-// import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.jdbc.core.JdbcTemplate;
 
-// Added to deal with CORS error on desktop
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-
-// implements CommandLineRunner
 @SpringBootApplication
 public class NeighborhoodDoctorsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NeighborhoodDoctorsApplication.class, args);
-	}
-
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-		    @Override
-		    public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/**").allowedOrigins("*");
-		    }
-		};
 	}
 }
