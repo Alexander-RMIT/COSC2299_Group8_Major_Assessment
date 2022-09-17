ae49f714cf464b7bf875e338377843f1e9ecaab0
Alexander-RMIT <82481042+Alexander-RMIT@users.noreply.github.com>
2022-09-17 11:40:01 +1000

    Update navigationBarPat.dart

--- FrontEnd/neighborhood_doctor/lib/pages/patient/navigationBarPat.dart
+++ FrontEnd/neighborhood_doctor/lib/pages/patient/navigationBarPat.dart
@@ -1,41 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:neighborhood_doctors/pages/patient/chatPat.dart';
+import 'package:neighborhood_doctors/pages/patient/patientHealthInfoPatView.dart';
 
+// Navigation bar specific to the general landing page that doesnt have user heirarchy
 
-class NavigationBarPat extends StatefulWidget{
+class NavigationBarLanding extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
-    return NavBarStatePat();
+    return NavBarLandingState();
   }
 }
 
-class NavBarStatePat extends State<NavigationBarPat> {
+class NavBarLandingState extends State<NavigationBarLanding> {
   final minimumPadding = 5.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Neighborhood Doctors Pages')
+          title: Text('Neighborhood Doctors Pages')
       ),
       body: Center(child: Text('Welcome to the Neighborhood Doctors website')),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.only(top: minimumPadding, bottom: minimumPadding),
           children: <Widget>[
-            DrawerHeader(
-              child: Text('Neighborhood Doctor management'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
+            const SizedBox(
+              height: 84,
+              child: DrawerHeader(
+                child: Text('Neighborhood Doctor management'),
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                ),
               ),
             ),
             ListTile(
-              title: Text('Calendar'),
+              title: Text('Chat'),
               onTap: () {
-                  Navigator.push(
-                    context,
+                Navigator.push(context,
                     MaterialPageRoute(
-                      builder: (context) => landing_user();
-                    )
-                  ),
+                        builder: (context) => ChatPatient(title: 'Chat')));
+              },
+            ),
+            ListTile(
+              title: Text('View health information'),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (context) => PatientHealthInfo(title: 'Health Information')));
               },
             )
           ],
