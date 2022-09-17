231aafa9b27bab411e7d37d4a2983a63861363f1
Alexander-RMIT <82481042+Alexander-RMIT@users.noreply.github.com>
2022-09-17 10:50:16 +1000

    Update patientHealthInfoPatView.dart

---
+++ FrontEnd/neighborhood_doctor/lib/pages/patient/patientHealthInfoPatView.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+
+class PatientHealthInfo extends StatefulWidget {
+  const PatientHealthInfo({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<PatientHealthInfo> createState() => PatientHealthInfoState();
+}
+
+class PatientHealthInfoState extends State<PatientHealthInfo> {
+  final _formKey = GlobalKey<FormState>();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Neighborhood Doctors Pages')
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Sign up',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                ),
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          // Add fields here
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
