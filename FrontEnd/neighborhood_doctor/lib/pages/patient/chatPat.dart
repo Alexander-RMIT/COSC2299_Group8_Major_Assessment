4d3d90fa030d4c07c91a8e4a4b2c5e9986c356df
Alexander-RMIT <82481042+Alexander-RMIT@users.noreply.github.com>
2022-09-17 14:55:56 +1000

    Add files via upload

---
+++ FrontEnd/neighborhood_doctor/lib/pages/patient/chatPat.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+
+class ChatPatient extends StatefulWidget {
+  const ChatPatient({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<ChatPatient> createState() => ChatPatientState();
+}
+
+class ChatPatientState extends State<ChatPatient> {
+  final _formKey = GlobalKey<FormState>();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Neighborhood Doctors Pages')
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Sign up',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                ),
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
