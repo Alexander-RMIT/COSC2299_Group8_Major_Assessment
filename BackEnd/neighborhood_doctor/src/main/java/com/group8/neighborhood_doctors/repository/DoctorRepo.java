9ac82d13822899b9a2673299216092924d274704
Alexander-RMIT <82481042+Alexander-RMIT@users.noreply.github.com>
2022-09-18 12:30:03 +1000

    Update DoctorRepo.java

--- BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/repository/DoctorRepo.java
+++ BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/repository/DoctorRepo.java
@@ -3,6 +3,7 @@ package com.group8.neighborhood_doctors.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import com.group8.neighborhood_doctors.doctor.Doctor;
 
@@ -18,4 +19,8 @@ public interface DoctorRepo extends JpaRepository<Doctor, Integer> {
 
     @Query("select max(s.id) from Doctor s")
     public Integer findMaxId();
+    
+    @Query("SELECT s.id FROM Doctor s WHERE s.email=:email AND s.password=:password")
+    public Integer findByEmailPassword(@Param("email") String email, 
+                                        @Param("password") String password);
 }
