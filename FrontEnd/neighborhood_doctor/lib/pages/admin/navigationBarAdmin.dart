790d735d80393c96667eea9f8a819e9d62f19c82
Alexander-RMIT <82481042+Alexander-RMIT@users.noreply.github.com>
2022-09-18 17:18:56 +1000

    Added welcome message

--- FrontEnd/neighborhood_doctor/lib/pages/admin/navigationBarAdmin.dart
+++ FrontEnd/neighborhood_doctor/lib/pages/admin/navigationBarAdmin.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'dart:convert';
 import 'package:neighborhood_doctors/pages/admin/createUser.dart';
+import 'package:http/http.dart' as http;
 
 class NavigationBarAdmin extends StatefulWidget{
   final int id;
@@ -16,6 +17,26 @@ class NavBarStateAdmin extends State<NavigationBarAdmin> {
   final int id;
   NavBarStateAdmin(this.id);
 
+  // Set at runtime instead of compile time
+  String _uname = "";
+  Future<String> userFirstName(int id, BuildContext context) async {
+    Uri urlAdminName = Uri.parse("http://10.0.2.2:8080/admin/username");
+
+    var response = await http.post(urlAdminName,
+        headers: <String, String>{"Content-Type": "application/json", },
+        body: jsonEncode(<String, dynamic>{
+          "id": id,
+        }));
+    String strResponse = response.body;
+
+    if (response.statusCode == 200) {
+      _uname = strResponse;
+      return strResponse;
+    } else {
+      return strResponse;
+    }
+  }
+
   final minimumPadding = 5.0;
   @override
   Widget build(BuildContext context) {
@@ -23,7 +44,17 @@ class NavBarStateAdmin extends State<NavigationBarAdmin> {
       appBar: AppBar(
           title: Text('Neighborhood Doctors Pages')
       ),
-      body: Center(child: Text('Welcome to the Neighborhood Doctors website')),
+      body: FutureBuilder<String>(
+        future: userFirstName(id, context),
+        builder: (username, context) {
+          if (username != "") {
+            String welcomeMsg = "Welcome to \nNeighborhood Doctors \n$_uname";
+            return Center(child: Text(welcomeMsg, textAlign: TextAlign.center));
+          } else {
+            return Center(child: Text("Welcome to Neighborhood Doctors"));
+          }
+        },
+      ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.only(top: minimumPadding, bottom: minimumPadding),
