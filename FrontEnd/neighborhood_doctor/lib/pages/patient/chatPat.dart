26dcc7bc687ee8138500f66bd1064beac7ec2f0c
Ethan-Rayner <s3902240@student.rmit.edu.au>
2022-09-20 23:18:19 +1000

    chat basic sending to database working

--- FrontEnd/neighborhood_doctor/lib/pages/patient/chatPat.dart
+++ FrontEnd/neighborhood_doctor/lib/pages/patient/chatPat.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ChatPatient extends StatefulWidget {
   const ChatPatient({Key? key, required this.id, required this.title})
@@ -6,11 +8,18 @@ class ChatPatient extends StatefulWidget {
   final int id;
   final String title;
   @override
-  State<ChatPatient> createState() => ChatPatientState();
+  State<StatefulWidget> createState() {
+    return ChatPatientState(this.id, this.title);
+  }
 }
 
 class ChatPatientState extends State<ChatPatient> {
+  final int id;
+  final String title;
   final _formKey = GlobalKey<FormState>();
+  ChatPatientState(this.id, this.title);
+  TextEditingController messageController = TextEditingController();
+  TextEditingController useroneController = TextEditingController();
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,20 +32,55 @@ class ChatPatientState extends State<ChatPatient> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              const Text(
-                'Sign up',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 40,
-                ),
-              ),
-              const SizedBox(
-                height: 60,
-              ),
+              ElevatedButton(
+                child: Text('Create Chat'),
+                onPressed: () {
+                  openChatDialog();
+                },
+              )
             ],
           ),
         ),
       ),
     );
+  }
+
+  Future openChatDialog() => showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+          title: Text("New Chat"),
+          content: Column(children: <Widget>[
+            TextField(
+                controller: messageController,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.timer),
+                  labelText: 'Enter Message',
+                )),
+            TextField(
+                controller: useroneController,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.timer),
+                  labelText: 'Enter doctorId',
+                )),
+            ElevatedButton(
+              child: Text('Send Chat'),
+              onPressed: () {
+                createChat(messageController.text,
+                    int.parse(useroneController.text), id);
+              },
+            )
+          ])));
+
+  Future<void> createChat(String message, int user1, int user2) async {
+    Uri url = Uri.parse("http://10.0.2.2:8080/chat/createChat");
+    var response = await http.post(url,
+        headers: <String, String>{
+          "Content-Type": "application/json",
+        },
+        body: jsonEncode(<String, dynamic>{
+          "message": message,
+          "userone": user1,
+          "usertwo": user2
+        }));
   }
 }
