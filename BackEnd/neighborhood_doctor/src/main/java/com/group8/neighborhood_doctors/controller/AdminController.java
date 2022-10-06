f051928654cf7867c17139d685ab59984bcc4425
Marco Lai <pakyinlai15@gmail.com>
2022-10-06 22:23:23 +1100

    Admin Controller update

--- BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/controller/AdminController.java
+++ BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/controller/AdminController.java
@@ -1,6 +1,10 @@
 package com.group8.neighborhood_doctors.controller;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import com.group8.neighborhood_doctors.administrator.Administrator;
+import com.group8.neighborhood_doctors.jwt.JwtUtility;
 
 import com.group8.neighborhood_doctors.service.AdminService;
 
@@ -10,7 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Base64;
 import java.util.List;
+
+import static com.group8.neighborhood_doctors.jwt.SecurityConstant.TOKEN_PREFIX;
+import org.json.simple.JSONObject;
 
 /*
     ================================
@@ -71,7 +79,34 @@ public class AdminController {
      */
     @RequestMapping(value = "auth/admin/login", method = RequestMethod.POST)
     public String loginAdmin(@RequestBody Administrator admin) {
-        return adminService.findAdmin(admin);
+        // return adminService.findAdmin(admin);
+
+        if (adminService.findAdmin(admin) == "Incorrect login credentials entered") {
+            return "Incorrect login credentials entered";
+        }
+
+        // JWT Token logging in
+        Base64.Encoder encode = Base64.getEncoder();
+
+        // Header
+        JSONObject header_raw = new JSONObject();
+        header_raw.put("alg", "HS256");
+        header_raw.put("type", "auth");
+        String header = encode.encodeToString(header_raw.toString().getBytes());
+
+        // Payload
+        JSONObject payload_raw = new JSONObject();
+        payload_raw.put("id", adminService.retrieveId(admin));
+        payload_raw.put("email", admin.getEmail());
+        payload_raw.put("password", admin.getPassword());
+        String payload = encode.encodeToString(payload_raw.toString().getBytes());
+
+        JwtUtility util = new JwtUtility();
+        
+        // Making token with signature
+        String JWT = TOKEN_PREFIX + util.HS256(header, payload);
+
+        return JWT;
     }
 
     /*
@@ -86,7 +121,22 @@ public class AdminController {
      * [POST] Get Administrator Username
      */
     @RequestMapping(value = "admin/username", method = RequestMethod.POST)
-    public String sessionAdminFname(@RequestBody int id) {
-        return adminService.retrieveUsername(id);
+    public String sessionAdminFname(@RequestBody String token) {
+        JwtUtility util = new JwtUtility();
+        
+        if (util.verifyToken(token)) {
+            String token_contents[] = token.split("\\.");
+            Base64.Decoder decode = Base64.getDecoder();
+            String payload = new String(decode.decode(token_contents[1].getBytes()));
+            JsonObject json = JsonParser.parseString(payload).getAsJsonObject();
+            System.out.println(json);
+            int id = json.get("id").getAsInt();
+            System.out.println("ID VALUE:" + id);
+
+            System.out.println("NAME:" + adminService.retrieveUsername(id));
+            return adminService.retrieveUsername(id);
+        } else {
+            return "";
+        }
     }
 }
