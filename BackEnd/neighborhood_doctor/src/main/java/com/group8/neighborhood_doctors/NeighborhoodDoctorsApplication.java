e175f8257ad21a0f5461c1dcdec748597fa43254
Chrisio_Gwaan <chris322322@gmail.com>
2022-10-06 22:06:33 +1100

    Removed unused codes

--- BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/NeighborhoodDoctorsApplication.java
+++ BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/NeighborhoodDoctorsApplication.java
@@ -2,7 +2,6 @@ package com.group8.neighborhood_doctors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NeighborhoodDoctorsApplication {
