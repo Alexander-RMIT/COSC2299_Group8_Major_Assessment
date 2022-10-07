a46db168b75a1f55b832b5788b1a64acaf86e8a9
Chrisio_Gwaan <chris322322@gmail.com>
2022-10-07 19:34:34 +1100

    Uncomment the non-use multi-config handling

--- BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/WebConfig.java
+++ BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/WebConfig.java
@@ -1,6 +1,6 @@
 package com.group8.neighborhood_doctors;
 
-import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /*
  * This class is used to enable CORS for the whole application.
  */
-@ComponentScan(basePackages = {"com.group8.neighborhood_doctors"}) // Handle multiple config classes
+// @ComponentScan(basePackages = {"com.group8.neighborhood_doctors"}) // Handle multiple config classes
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
