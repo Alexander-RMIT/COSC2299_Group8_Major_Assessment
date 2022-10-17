255460c7f10889c56c825b0f8ccb822262d254d8
Chrisio_Gwaan <chris322322@gmail.com>
2022-10-17 20:22:43 +1100

    Added welcome message on the backend main page

--- BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/NeighborhoodDoctorsApplication.java
+++ BackEnd/neighborhood_doctor/src/main/java/com/group8/neighborhood_doctors/NeighborhoodDoctorsApplication.java
@@ -2,11 +2,19 @@ package com.group8.neighborhood_doctors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NeighborhoodDoctorsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NeighborhoodDoctorsApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Welcome! This is a Spring Boot application for Neighborhood Doctors!";
 	}
 }
